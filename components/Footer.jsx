5267cadd155502ada0b3da1f216ac31fa097fdfa
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-01-23 13:51:30 +0200

    Update contacts

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,24 @@
+/* eslint-disable @next/next/link-passhref */
+/* eslint-disable react/jsx-no-undef */
 import React from 'react';
+import Link from 'next/link';
 import { AiFillInstagram, AiFillFacebook } from 'react-icons/ai';
+import { RiWhatsappFill } from 'react-icons/ri';
 
 const Footer = () => {
 	return (
 		<div className="footer-container">
 			<p>2022 MotoHai echipamente moto All rights reserved</p>
 			<p className="icons">
-				<AiFillFacebook />
-				<AiFillInstagram />
+				<Link href="https://www.facebook.com/profile.php?id=100089325766237">
+					<AiFillFacebook />
+				</Link>
+				<Link href="https://www.instagram.com/motohai.ro/">
+					<AiFillInstagram />
+				</Link>
+				<Link href="https://wa.me/+40742362752">
+					<RiWhatsappFill />
+				</Link>
 			</p>
 		</div>
 	);
