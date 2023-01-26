27813f4e7933c478032f64a6c4d551456816b7e7
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-01-26 15:22:13 +0200

    Update styles for banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -7,26 +7,27 @@ import { urlFor } from '../lib/client';
 const HeroBanner = ({ heroBanner }) => {
 	return (
 		<div className="hero-banner-container">
-			<div>
-				<p className="beats-solo">{heroBanner.smallText}</p>
+			<div className="details-container">
+				<div>
+					<p className="beats-solo">{heroBanner.smallText}</p>
+				</div>
+				<h3>{heroBanner.midText}</h3>
+				<h1>{heroBanner.largeText1}</h1>
+				<div className="desc">
+					<h5>Descriere</h5>
+					<p>{heroBanner.desc}</p>
+				</div>
+				<div>
+					<Link href={`/product/${heroBanner.product}`}>
+						<button type="button">{heroBanner.buttonText}</button>
+					</Link>
+				</div>
 			</div>
-			<h3>{heroBanner.midText}</h3>
-			<h1>{heroBanner.largeText1}</h1>
 			<img
 				src={urlFor(heroBanner.image).url()}
 				alt="casca"
 				className="hero-banner-image"
 			/>
-
-			<div>
-				<Link href={`/product/${heroBanner.product}`}>
-					<button type="button">{heroBanner.buttonText}</button>
-				</Link>
-				<div className="desc">
-					<h5>Descriere</h5>
-					<p>{heroBanner.desc}</p>
-				</div>
-			</div>
 		</div>
 	);
 };
